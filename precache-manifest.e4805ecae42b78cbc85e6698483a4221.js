65263088834d1015da1d1f71e7cd94d9caf0f59d
Karthikeyan Kannan <karthikeyan.k.87@gmail.com>
2020-09-12 12:15:34 +0530

    Updates

---
+++ precache-manifest.e4805ecae42b78cbc85e6698483a4221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75552cf254c46d1f63ef8514c420ccba",
+    "url": "/roboters-react-app/index.html"
+  },
+  {
+    "revision": "a31a29d35cde3f599eb6",
+    "url": "/roboters-react-app/static/css/main.944f8911.chunk.css"
+  },
+  {
+    "revision": "5329ff387f657b6c2e5f",
+    "url": "/roboters-react-app/static/js/2.7b96187e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboters-react-app/static/js/2.7b96187e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31a29d35cde3f599eb6",
+    "url": "/roboters-react-app/static/js/main.fd4c2d28.chunk.js"
+  },
+  {
+    "revision": "374e810f15e1e1f1b8c8",
+    "url": "/roboters-react-app/static/js/runtime-main.c7dfa105.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roboters-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
